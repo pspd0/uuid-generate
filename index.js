a565fd69e95be6ae06df997a3938cf1f0d8b462c
ryurack <73247894+pspd0@users.noreply.github.com>
2022-08-31 18:52:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function Generate(count) {
+    let items = [];
     function uuid() {
         const hex = "0123456789ABCDEFG";
         const model = "xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx";
@@ -6,23 +7,23 @@ function Generate(count) {
         for (var i = 0; i < model.length; i++) {
             var random = Math.floor(Math.random() * hex.length);
             str += model[i] == "x" ?  hex[random] : model[i] ;
-        }
+        };
         return str.toLowerCase();
     };
 
     if(count == null) {
-        uuid();
+        items = {uuid: uuid()};
+        return items
     } else {
         if(typeof count == 'number') {
             if(count == 0) throw new Error("Count is 0");
-            let items = [];
             for(let i = 0; i<count; i++) {
                 items[i] = {uuid: uuid()};
             };
 
             return items
         };
-    }
-}
+    };
+};
  
-console.log(Generate(5));
+console.log(Generate(10));
